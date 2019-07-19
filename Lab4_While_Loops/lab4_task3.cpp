90b5f499ce780ccd599acd84e967f40987d445f0
Johneill <44747107+Johneill@users.noreply.github.com>
2019-07-19 09:27:28 +0100

    Add files via upload

---
+++ Lab4_While_Loops/lab4_task3.cpp
@@ -0,0 +1,60 @@
+//
+//  lab4_task3.cpp
+//  
+//
+//  Created by Lucy Byrne on 08/02/2017.
+//
+//
+
+#include "lab4_task3.hpp"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    double max, min, temp, average, sum, count;
+    string name;
+    
+    cin >> name;
+    
+    while (name != "XXX") {
+    
+    
+    cin >> temp;
+    max = temp;
+    min = temp;
+    average = 0;
+    sum = 0;
+    count = 0;
+   
+    
+    
+    while (temp != -999) {
+        
+        count = count + 1;
+        sum = sum + temp;
+        if (temp > max)
+            max = temp;
+        
+        
+        if (temp < min){
+            min = temp;
+        }
+        cin >> temp;
+        
+    }
+    
+    average = sum/count;
+    cout << "Name: " << name << endl;
+    cout << "Average: " << average << endl;
+    cout << "Max: " << max << " " << "Min: " << min << endl;
+    
+        cin >> name;
+    }
+    
+    
+    
+    
+    return 0;
+    
+}
